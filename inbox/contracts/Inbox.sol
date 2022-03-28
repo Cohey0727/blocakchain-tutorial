93aa895346494d4b1cf1921008fba4562c3049c1
Kohei <ohayousagi.ac.kook0727@gmail.com>
2022-03-28 17:11:35 +0900

    master

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.4.17;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
 // linter warnings (red underline) about pragma version can igonored!
 
 // contract code will go here
