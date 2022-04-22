199d7e63a5dc3c16173e9fb4ee88ecf81a287f98
Kohei <ohayousagi.ac.kook0727@gmail.com>
2022-04-22 15:10:07 +0900

    main

--- lottery-frontend/src/Layout.tsx
+++ lottery-frontend/src/Layout.tsx
@@ -2,8 +2,6 @@ import { Layout, Menu, Breadcrumb, MenuProps } from "antd";
 import {
   DesktopOutlined,
   PieChartOutlined,
-  FileOutlined,
-  TeamOutlined,
   UserOutlined,
 } from "@ant-design/icons";
 import React, { useState } from "react";
@@ -12,33 +10,18 @@ const { Header, Content, Footer, Sider } = Layout;
 
 type MenuItem = Required<MenuProps>["items"][number];
 
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
+function getItem(label: React.ReactNode, icon?: React.ReactNode): MenuItem {
   return {
-    key,
+    key: label,
     icon,
-    children,
     label,
   } as MenuItem;
 }
 
 const items: MenuItem[] = [
-  getItem("Option 1", "1", <PieChartOutlined />),
-  getItem("Option 2", "2", <DesktopOutlined />),
-  getItem("User", "sub1", <UserOutlined />, [
-    getItem("Tom", "3"),
-    getItem("Bill", "4"),
-    getItem("Alex", "5"),
-  ]),
-  getItem("Team", "sub2", <TeamOutlined />, [
-    getItem("Team 1", "6"),
-    getItem("Team 2", "8"),
-  ]),
-  getItem("Files", "9", <FileOutlined />),
+  getItem("My Page", <PieChartOutlined />),
+  getItem("Entry Lottery", <DesktopOutlined />),
+  getItem("Lottery Result", <UserOutlined />),
 ];
 
 function SiderDemo() {
@@ -69,9 +52,7 @@ function SiderDemo() {
             Bill is a cat.
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
+        <Footer style={{ textAlign: "center" }}>Ether Lottery ©2022</Footer>
       </Layout>
     </Layout>
   );
